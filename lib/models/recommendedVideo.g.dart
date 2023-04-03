fa09024fe7046262032137c0ed8886226f69cc51
Paul Fauchon <paulfauchon@xendit.co>
2023-04-03 17:36:08 +0800

    add auto-play recommended option on videos

--- lib/models/recommendedVideo.g.dart
+++ lib/models/recommendedVideo.g.dart
@@ -13,17 +13,21 @@ RecommendedVideo _$RecommendedVideoFromJson(Map<String, dynamic> json) =>
       (json['videoThumbnails'] as List<dynamic>)
           .map((e) => ImageObject.fromJson(e as Map<String, dynamic>))
           .toList(),
-      json['author'] as String,
+      json['author'] as String?,
       json['lengthSeconds'] as int,
       json['viewCountText'] as String,
-    );
+    )
+      ..authorId = json['authorId'] as String?
+      ..authorUrl = json['authorUrl'] as String?;
 
 Map<String, dynamic> _$RecommendedVideoToJson(RecommendedVideo instance) =>
     <String, dynamic>{
-      'videoId': instance.videoId,
       'title': instance.title,
-      'videoThumbnails': instance.videoThumbnails,
-      'author': instance.author,
+      'videoId': instance.videoId,
       'lengthSeconds': instance.lengthSeconds,
+      'author': instance.author,
+      'authorId': instance.authorId,
+      'authorUrl': instance.authorUrl,
+      'videoThumbnails': instance.videoThumbnails,
       'viewCountText': instance.viewCountText,
     };
