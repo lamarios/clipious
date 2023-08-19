a5ec22919cc9edca6e02c45b9ec03062bc9cd04b
Paul Fauchon <paul@fauchon.dev>
2023-08-19 15:03:27 +0800

    fix issue that makes live streams not playable

--- lib/player/views/components/mini_player_progress.dart
+++ lib/player/views/components/mini_player_progress.dart
@@ -13,25 +13,27 @@ class MiniPlayerProgress extends StatelessWidget {
       buildWhen: (previous, current) => previous.position != current.position,
       builder: (context, _) {
         var player = context.read<PlayerCubit>();
-        return Container(
-            alignment: Alignment.centerLeft,
-            width: double.infinity,
-            height: 2,
-            decoration: BoxDecoration(
-              color: colors.secondaryContainer,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: AnimatedFractionallySizedBox(
-                widthFactor: _.position.inMilliseconds / player.duration.inMilliseconds,
-                heightFactor: 1,
-                duration: const Duration(milliseconds: 750),
-                curve: Curves.easeInOutQuad,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: colors.primary,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                )));
+        return !(player.state.currentlyPlaying?.liveNow ?? false)
+            ? Container(
+                alignment: Alignment.centerLeft,
+                width: double.infinity,
+                height: 2,
+                decoration: BoxDecoration(
+                  color: colors.secondaryContainer,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: AnimatedFractionallySizedBox(
+                    widthFactor: _.position.inMilliseconds / player.duration.inMilliseconds,
+                    heightFactor: 1,
+                    duration: const Duration(milliseconds: 750),
+                    curve: Curves.easeInOutQuad,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: colors.primary,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    )))
+            : const SizedBox.shrink();
       },
     );
   }
