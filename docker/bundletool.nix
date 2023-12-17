e898772746dcbd084bc2c9e0cca5ee37469fa547
office-pc nix root <paul@fauchon.dev>
2023-12-17 18:18:03 +0800

    add aapt to bundletool image

---
+++ docker/bundletool.nix
@@ -0,0 +1,13 @@
+# usage:
+# build the image
+# nix-build -o result bundletool.nix
+# docker load -i result
+# use the image as usual with name gonzague/bundletool
+{
+  pkgs ? import <nixpkgs> {system = "x86_64-linux";}
+}:
+pkgs.dockerTools.buildLayeredImage {
+  name = "gonzague/bundletool";
+  tag = "latest";
+  contents = with pkgs; [ wget bundletool aapt ];
+}
