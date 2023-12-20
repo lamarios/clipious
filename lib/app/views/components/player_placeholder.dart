f0b7b86a4adc600087f0f9daa3b4a4cdbdd65df2
Paul Fauchon <paulfauchon@xendit.co>
2023-12-20 10:58:49 +0800

    add icon to player placeholder

--- lib/app/views/components/player_placeholder.dart
+++ lib/app/views/components/player_placeholder.dart
@@ -11,22 +11,30 @@ class PlayerPlaceHolder extends StatelessWidget {
     var colors = Theme.of(context).colorScheme;
     return Material(child: Builder(
       builder: (context) {
-        var showMiniPlaceholder = context
-            .select((PlayerCubit value) => value.state.showMiniPlaceholder);
-        var isMini = context.select((PlayerCubit value) => value.state.isMini);
+        var showPlaceHolder =
+            context.select((PlayerCubit value) => value.state.showMiniPlaceholder && value.state.isMini);
         return AnimatedOpacity(
-          opacity: showMiniPlaceholder && isMini ? 1 : 0,
+          opacity: showPlaceHolder ? 1 : 0,
           duration: animationDuration,
           child: AnimatedContainer(
               duration: animationDuration,
               curve: Curves.easeInOut,
-              height: isMini && showMiniPlaceholder ? targetHeight : 0,
+              height: showPlaceHolder ? targetHeight : 0,
               color: colors.background,
               padding: const EdgeInsets.symmetric(horizontal: 7),
               child: Container(
                 decoration: BoxDecoration(
                   color: colors.secondaryContainer.withOpacity(0.5),
                   borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: AnimatedOpacity(
+                      duration: animationDuration * 4,
+                      opacity: showPlaceHolder ? 1 : 0,
+                      child: Icon(
+                        Icons.play_arrow_outlined,
+                        color: colors.onSecondaryContainer.withOpacity(0.3),
+                      )),
                 ),
               )),
         );
