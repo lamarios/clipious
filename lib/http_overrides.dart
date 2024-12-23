f64b4c0b3393c7e8b5a683c868ca961029041a80
office-pc nix root <paul@fauchon.dev>
2024-12-23 16:10:07 +0800

    add bundle id in user agent

--- lib/http_overrides.dart
+++ lib/http_overrides.dart
@@ -7,7 +7,8 @@ class MyHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..userAgent = 'Clipious/${packageInfo?.version ?? '0.0.0'}'
+      ..userAgent =
+          'Clipious/${packageInfo?.version ?? '0.0.0'} (${packageInfo?.packageName ?? ''})'
       ..badCertificateCallback = (X509Certificate cert, String host, int port) {
         // if true, it will allow all certs, if false it will throw error on a bad cert
         return db.getSettings(skipSslVerificationSettingName)?.value == 'true';
